ff457dd5ad0a73748edd3d6df816b763a7e8596c
Tingting Chen <tingtingmacpro@Tingtings-MacBook-Pro.local>
2020-07-06 14:01:35 -0700

    mlab pass

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
+
+
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/imaginary_traveler",
+    // process.env.MONGODB_URI || "mongodb://localhost/imaginary_traveler",
+    process.env.MONGODB_URI ||     "mongodb://mongo:35mongo35@ds143678.mlab.com:43678/heroku_vvhfchhr",
+
     {
         useNewUrlParser: true,
         useCreateIndex: true,
